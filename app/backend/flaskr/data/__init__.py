1c91d9e4ea53c3baa94d1958263b6273c635fd25
Ryan <ryanzhou7@gmail.com>
2022-10-08 09:25:30 -0700

    Finish

--- app/backend/flaskr/data/__init__.py
+++ app/backend/flaskr/data/__init__.py
@@ -2,8 +2,10 @@
 # They can be used by third party tools such as type checkers, IDEs, linters, etc.
 # https://docs.python.org/3/library/typing.html
 def get_long_url(short_url: str) -> str:
+    # TODO
     return 'long_url'
 
 
 def save_url(long_url: str) -> str:
+    # TODO
     return 'short_url'
